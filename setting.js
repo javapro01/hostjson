28203430ee1f2ca11c09d12e08f5fcc9e85a9edd
javapro01 <74463973+javapro01@users.noreply.github.com>
2020-11-18 16:12:24 +0100

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,15 @@
+{
+  "xvUsn": true,
+  "xvShowAds": true,
+  "xvShowInt": true,
+  "xvShowBan": true,
+  "xvShowAgres": true,
+  "xvIdPubli": "111111111111",
+  "xvIdBan": "51985493",
+  "xvIdInter": "99999999999999",
+  "xvForceUpdate": true,
+  "xvCurrentVersion": 1,
+  "xvPack": "com.tat.ssa",
+  "xvUpdateUrl": "market://details?id=com.",
+  "xvUpdateUrlCatch": "https://www.wwwwwwwwww"
+}
