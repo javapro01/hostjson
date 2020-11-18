d333ca2ceb5cb5bab381e725f2cb8e9a93175da8
javapro01 <74463973+javapro01@users.noreply.github.com>
2020-11-18 22:22:02 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -8,7 +8,7 @@
   "xvIdPubli": "3940256099942544",
   "xvIdBan": "51985493",
   "xvIdInter": "8691691433",
-  "xvForceUpdate": true,
+  "xvForceUpdate": false,
   "xvCurrentVersion": 2,
   "xvPack": "com.tat.ssa",
   "xvUpdateUrl": "https://www.google.com",
